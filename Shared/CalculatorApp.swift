e100f4fa552d2ac8a791c906f7cde72e2b2b1ed7
nikki <nfhisey@gmail.com>
2022-01-30 16:24:37 -0800

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Nicole Hisey on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
